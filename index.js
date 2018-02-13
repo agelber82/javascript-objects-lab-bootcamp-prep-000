eeda48ea7493584519d0bb3cbfcfeb442de496ec
alandgelber@gmail.com <alandgelber@gmail.com>
2018-02-13 16:49:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var recipes = { foo: 'bar' };
 var newObj = Object.assign({}, obj);
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return object.recipes{}, object, { [key]: value });
+  return object.recipes({}, object, { [key]: value });
 }
